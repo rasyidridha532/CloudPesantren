304d4634aa3b5ae772ffcd6ff3fc4307abe4bec5
M. Rasyid Ridha <mrasyidridha532@gmail.com>
2020-07-20 17:33:56 +0700

    file

--- application/controllers/File.php
+++ application/controllers/File.php
@@ -22,14 +22,6 @@ class File extends CI_Controller
         $q = urldecode($this->input->get('q', TRUE));
         $start = intval($this->input->get('start'));
 
-        if ($q <> '') {
-            $config['base_url'] = base_url() . 'file/index.html?q=' . urlencode($q);
-            $config['first_url'] = base_url() . 'file/index.html?q=' . urlencode($q);
-        } else {
-            $config['base_url'] = base_url() . 'file/index.html';
-            $config['first_url'] = base_url() . 'file/index.html';
-        }
-
         $config['per_page'] = 10;
         $config['page_query_string'] = TRUE;
         $config['total_rows'] = $this->File_model->total_rows($q);
@@ -38,46 +30,72 @@ class File extends CI_Controller
         $this->load->library('pagination');
         $this->pagination->initialize($config);
 
+        $fotoprofil = $this->session->userdata('gambar');
+        $nama = $this->session->userdata('nama');
+        $role = $this->session->userdata('role');
+
         $data = array(
             'file_data' => $file,
             'q' => $q,
             'pagination' => $this->pagination->create_links(),
             'total_rows' => $config['total_rows'],
             'start' => $start,
+            'foto' => $fotoprofil,
+            'nama' => $nama,
+            'role' => $role,
+            'title' => 'File'
         );
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar');
         $this->load->view('file/tbl_file_list', $data);
-    }
-
-    public function read($id)
-    {
-        $row = $this->File_model->get_by_id($id);
-        if ($row) {
-            $data = array(
-                'id_file' => $row->id_file,
-                'judul' => $row->judul,
-                'nama_file' => $row->nama_file,
-                'size' => $row->size,
-                'uploaded_at' => $row->uploaded_at,
-            );
-            $this->load->view('file/tbl_file_read', $data);
-        } else {
-            $this->session->set_flashdata('message', 'Record Not Found');
-            redirect(site_url('file'));
-        }
+        $this->load->view('template/footer');
     }
 
     public function create()
     {
+        $fotoprofil = $this->session->userdata('gambar');
+        $nama = $this->session->userdata('nama');
+        $role = $this->session->userdata('role');
+
         $data = array(
             'button' => 'Create',
             'action' => site_url('file/create_action'),
             'id_file' => set_value('id_file'),
             'judul' => set_value('judul'),
-            'nama_file' => set_value('nama_file'),
-            'size' => set_value('size'),
-            'uploaded_at' => set_value('uploaded_at'),
+            'foto' => $fotoprofil,
+            'nama' => $nama,
+            'role' => $role,
+            'title' => 'Upload File'
         );
+        $this->load->view('template/header', $data);
+        $this->load->view('template/sidebar');
         $this->load->view('file/tbl_file_form', $data);
+        $this->load->view('template/footer');
+    }
+
+    public function update($id)
+    {
+        $fotoprofil = $this->session->userdata('gambar');
+        $nama = $this->session->userdata('nama');
+        $role = $this->session->userdata('role');
+
+        $row = $this->File_model->get_by_id($id);
+
+        if ($row) {
+            $data = array(
+                'button' => 'Update',
+                'foto' => $fotoprofil,
+                'nama' => $nama,
+                'role' => $role,
+                'action' => site_url('file/update_action' . $row->id_file),
+                'id_file' => set_value('id_file', $row->id_file),
+                'judul' => set_value('judul', $row->judul),
+            );
+            $this->load->view('file/tbl_file_form', $data);
+        } else {
+            $this->session->set_flashdata('message', 'Record Not Found');
+            redirect(site_url('file'));
+        }
     }
 
     public function create_action()
@@ -87,54 +105,42 @@ class File extends CI_Controller
         if ($this->form_validation->run() == FALSE) {
             $this->create();
         } else {
+            $file_spec = $this->_upload_file();
+            $nama_file = $file_spec['namafile'];
+            $size = $file_spec['size'];
             $data = array(
                 'judul' => $this->input->post('judul', TRUE),
-                'nama_file' => $this->input->post('nama_file', TRUE),
-                'size' => $this->input->post('size', TRUE),
-                'uploaded_at' => $this->input->post('uploaded_at', TRUE),
+                'nama_file' => $nama_file,
+                'size' => $size
             );
 
             $this->File_model->insert($data);
-            $this->session->set_flashdata('message', 'Create Record Success');
+            $this->session->set_flashdata('message', '<div class="alert alert-success">File berhasil diupload!</div>');
             redirect(site_url('file'));
         }
     }
 
-    public function update($id)
-    {
-        $row = $this->File_model->get_by_id($id);
-
-        if ($row) {
-            $data = array(
-                'button' => 'Update',
-                'action' => site_url('file/update_action'),
-                'id_file' => set_value('id_file', $row->id_file),
-                'judul' => set_value('judul', $row->judul),
-                'nama_file' => set_value('nama_file', $row->nama_file),
-                'size' => set_value('size', $row->size),
-                'uploaded_at' => set_value('uploaded_at', $row->uploaded_at),
-            );
-            $this->load->view('file/tbl_file_form', $data);
-        } else {
-            $this->session->set_flashdata('message', 'Record Not Found');
-            redirect(site_url('file'));
-        }
-    }
-
-    public function update_action()
+    public function update_action($id)
     {
         $this->_rules();
+
+        $row = $this->File_model->get_by_id($id);
+        $old_file = $row->nama_file;
 
         if ($this->form_validation->run() == FALSE) {
             $this->update($this->input->post('id_file', TRUE));
         } else {
+            $filename = $this->_upload_file();
+            $nama_file = $filename['namafile'];
+            $size = $filename['size'];
+
             $data = array(
                 'judul' => $this->input->post('judul', TRUE),
-                'nama_file' => $this->input->post('nama_file', TRUE),
-                'size' => $this->input->post('size', TRUE),
-                'uploaded_at' => $this->input->post('uploaded_at', TRUE),
+                'nama_file' => $nama_file,
+                'size' => $size
             );
 
+            unlink('./uploads/file/opop/' . $old_file);
             $this->File_model->update($this->input->post('id_file', TRUE), $data);
             $this->session->set_flashdata('message', 'Update Record Success');
             redirect(site_url('file'));
@@ -147,28 +153,46 @@ class File extends CI_Controller
 
         if ($row) {
             $this->File_model->delete($id);
-            $this->session->set_flashdata('message', 'Delete Record Success');
+            $this->session->set_flashdata('message', '<div class="alert alert-success">File Berhasil Dihapus!</div>');
             redirect(site_url('file'));
         } else {
-            $this->session->set_flashdata('message', 'Record Not Found');
+            $this->session->set_flashdata('message', '<div class="alert alert-failed">Data Tidak Ditemukan!</div>');
             redirect(site_url('file'));
+        }
+    }
+
+    private function _upload_file()
+    {
+        $uploadFile = [];
+
+        $config['upload_path'] = './uploads/file/';
+        $config['allowed_types'] = 'pdf|doc|docx|xls|xlsx|ppt|pptx';
+        $config['max_size'] = 0;
+        $config['file_name'] = 'Pesantren-' . date('dmy') . '-' . substr(md5(rand()), 0, 10);
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('file')) {
+            $this->session->set_flashdata('message', '<div class="alert alert-failed">File tidak support!</div>');
+            redirect(site_url('file/create'));
+        } else {
+            $fileData = $this->upload->data();
+            $uploadFile['namafile'] = $fileData['file_name'];
+            $uploadFile['size'] = $fileData['file_size'];
+        }
+
+        if (!empty($uploadFile)) {
+            return $uploadFile;
         }
     }
 
     public function _rules()
     {
-        $this->form_validation->set_rules('judul', 'judul', 'trim|required');
-        $this->form_validation->set_rules('nama_file', 'nama file', 'trim|required');
-        $this->form_validation->set_rules('size', 'size', 'trim|required');
-        $this->form_validation->set_rules('uploaded_at', 'uploaded at', 'trim|required');
+        $this->form_validation->set_rules('judul', 'judul', 'trim|required' . [
+            'required' => 'Nama File Harus Diisi!'
+        ]);
 
         $this->form_validation->set_rules('id_file', 'id_file', 'trim');
         $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
     }
 }
-
-/* End of file File.php */
-/* Location: ./application/controllers/File.php */
-/* Please DO NOT modify this information : */
-/* Generated by Harviacode Codeigniter CRUD Generator 2020-07-20 07:04:27 */
-/* http://harviacode.com */
